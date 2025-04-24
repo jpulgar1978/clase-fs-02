69fb4ba75870b4d98210578864fa335fc2386811
Juan Pulgar <jpulgar1978@gmail.com>
2025-04-24 13:41:38 -0400

    primer servicio de alumnos

---
+++ src/main/java/com/clase/clase/controller/AlumnoController.java
@@ -0,0 +1,14 @@
+package com.clase.clase.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class AlumnoController {
+    @GetMapping("/api/v1/alumnos")
+    public String getAlumnos() {
+        return "LISTADO DE ALUMNOS";
+    }
+    
+}
